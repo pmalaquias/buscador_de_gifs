6e9cae87aed417fdbdd5fddeb597c90718a53f6d
pedro_igor <pedrode13souza@gmail.com>
2020-08-01 12:35:37 -0300

    Seach vazio

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -20,7 +20,7 @@ class _HomePageState extends State<HomePage> {
   Future<Map> _getGifs() async {
     http.Response response;
 
-    if (_search == null) {
+    if (_search == null || _search.isEmpty) {
       response = await http.get(
           "https://api.giphy.com/v1/gifs/trending?api_key=GTIQDiTy1930VtJGUAd7zNackvlEJQxF&limit=20&rating=g");
     } else {
