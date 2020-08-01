0d502ed9c786f7f7cc62b355a4e32435c4905128
pedro_igor <pedrode13souza@gmail.com>
2020-08-01 11:23:12 -0300

    Compartilanfo Gifs

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 import 'gif_page.dart';
+import 'package:share/share.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -127,6 +128,10 @@ class _HomePageState extends State<HomePage> {
                     MaterialPageRoute(
                         builder: (context) =>
                             GifPage(snapshot.data["data"][index])));
+              },
+              onLongPress: () {
+                Share.share(snapshot.data["data"][index]["images"]
+                    ["fixed_height"]["url"]);
               },
             );
           } //fim do if
